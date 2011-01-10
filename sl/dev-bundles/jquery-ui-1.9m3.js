a65a56544e54a8b35bc897b68634631b456b6590
Balazs Ree <ree@greenfinity.hu>
2011-01-10 17:20:56 +0100

    add jquery-ui development sources

---
+++ sl/dev-bundles/jquery-ui-1.9m3.js
@@ -0,0 +1,52 @@
+
+/**
+* jQuery UI 1.9m3 resource composition
+* You have to use juicer to produce a minified resource based on this file
+* You cannot use this file as a development resource
+*
+* Core files:
+*     @depends jquery-ui-1.9m3/external/jquery.bgiframe-2.1.2.js        
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.core.js        
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.widget.js       
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.mouse.js          
+*    
+* Behaviour:
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.dragable.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.droppable.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.resizable.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.selectable.js
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.sortable.js
+*    
+* Widgets:
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.accordion.js
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.autocomplete.js
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.button.js
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.datepicker.js        
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.dialog.js
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.menu.js
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.position.js        
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.progressbar.js
+*     @depends jquery-ui-1.9m3/ui/jquery.ui.slider.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.spinner.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.tabs.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.ui.tooltip.js
+* 
+* Effects:
+*     @depends jquery-ui-1.9m3/ui/jquery.effects.core.js        
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.blind.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.bounce.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.clip.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.core.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.drop.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.explode.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.fade.js
+*     @depends jquery-ui-1.9m3/ui/jquery.effects.fold.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.highlight.js
+*     @depends jquery-ui-1.9m3/ui/jquery.effects.pulsate.js        
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.scale.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.shake.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.slide.js
+*         @ NOT depends jquery-ui-1.9m3/ui/jquery.effects.transfer.js
+* 
+*/
+
