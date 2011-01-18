8bb6dc532d8f4b4e8581a3d3ff0158b396221960
Paul Everitt <paul@agendaless.com>
2011-01-18 15:17:35 -0500

    About to rename

--- bc.grid/demo.py
+++ bc.grid/demo.py
@@ -1,29 +1,30 @@
-from os.path import join, split
+from os.path import abspath, join, split
 
 from pyramid.config import Configurator
 from pyramid.response import Response
 from paste.httpserver import serve
 from pyramid.view import view_config
 
+from models import Bottlecap
+from views import sitemodel, hello
+
 def get_base():
-    return split(__file__)[0]
-
-@view_config(renderer="json")
-def sitemodel(request):
-    return [1,2,3]
-
-def hello_world(request):
-    return Response('Hello world!')
-
-def goodbye_world(request):
-    return Response('Goodbye world!')
+    return split(abspath(__file__))[0]
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_view(hello_world)
-    config.add_view(goodbye_world, name='goodbye')
-    config.add_route('sitemodel', '/static/bc.grid/sitemodel.json', view=sitemodel, view_renderer="json")
+
+    # The bottlecap entry point, some URL which can be introspected.
+    # We want to preserve Ajax dev using filesystem, so intercept
+    # a longer URL path.
+    config.add_route('sitemodel', '/static/bc.grid/sitemodel.json',
+                     view=sitemodel, view_renderer="json")
+
+    # Static resources
     static_path = split(get_base())[0]
     config.add_static_view(name='static', path=static_path)
+
+    # Now our Bottlecap views
+    #config.scan()
     app = config.make_wsgi_app()
     serve(app, host='0.0.0.0')
