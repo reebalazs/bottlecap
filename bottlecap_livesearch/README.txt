f10d414a50bf51cc424e64e0abe4a3ac87ac5c73
Tres Seaver <tseaver@palladion.com>
2011-01-27 11:48:11 -0500

    Add empty reources-as-egg package for livesearch.

---
+++ bottlecap_livesearch/README.txt
@@ -0,0 +1,5 @@
+``bottlecap_livesearch`` README
+===============================
+
+This package exists to provide a standard livesearch widget for ``bottlecap``
+and related applications.
---
+++ bottlecap_livesearch/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'bottlecap_core',
+]
+
+setup(name='bottlecap_livesearch',
+      version='0.1',
+      description='Bottlecap livesearch widget',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = requires,
+      tests_require= requires,
+      test_suite="bottlecap_livesearch.tests",
+      entry_points = """\
+      """,
+     )
