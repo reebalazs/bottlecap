62219599e40e6ce09fd0d8b786fb82f3fb7e5c89
Tres Seaver <tseaver@palladion.com>
2011-02-02 08:59:30 -0500

    Add IContainerInfo adapter for our application.

---
+++ bottlecap/bottlecap/adapters.py
@@ -0,0 +1,113 @@
+import datetime
+
+from pyramid.url import resource_url
+from pyramid.url import static_url
+from zope.interface import implements
+
+from bottlecap.interfaces import IContainerInfo
+from bottlecap.interfaces import IItemInfo
+
+_NOW = None
+def _now():
+    if _NOW is not None:
+        return _NOW
+    return datetime.datetime.now()
+
+class FolderContainerInfo(object):
+    implements(IContainerInfo)
+
+    def __init__(self, context):
+        self.context = context
+
+    title = property(lambda self: _get_title(self.context))
+
+    modified = property(lambda self: _get_modified(self.context))
+
+    creator = property(lambda self: getattr(self.context, 'creator', None))
+
+    def _get_actions(self):
+        #return [ViewActionInfo(self.context)]
+        return []
+    actions = property(_get_actions)
+
+    def _get_factories(self):
+        #return [FolderFactoryInfo(self.context)]
+        return []
+    factories = property(_get_factories)
+
+    # We don't currently support filtering or sorting
+    filter_schema = None
+    sort_schema = None
+
+    def parent_url(self, request):
+        parent = self.context.__parent__
+        if parent is not None:
+           return resource_url(parent, request)
+
+    def icon_url(self, request):
+        return static_url('bottlecap:static/folder_icon.png', request)
+
+    def listItems(self,
+                  registry,
+                  filter_spec=None,
+                  sort_spec=None,
+                  batch_start=None,
+                  batch_size=None,
+                 ):
+        if filter_spec is not None:
+            raise ValueError("'filter_spec' unsupported")
+        if sort_spec is not None:
+            raise ValueError("'sort_spec' unsupported")
+        if batch_size is not None:
+            if batch_start is None:
+                raise ValueError("'batch_size' requires also 'batch_start")
+        else:
+            if batch_start is not None:
+                batch_size = 20
+        values = self.context.values()
+        if batch_start is not None:
+            values = values[batch_start:batch_start+batch_size]
+        return filter(None, [registry.queryAdapter(x, IItemInfo)
+                                for x in values])
+
+    def __call__(self,
+                 request,
+                 filter_spec=None,
+                 sort_spec=None,
+                 batch_start=None,
+                 batch_size=None,
+                 include_actions=True,
+                 include_factories=True,
+                ):
+        result = {'title': self.title,
+                  'modified': self.modified,
+                  'creator': self.creator,
+                  'parent_url': self.parent_url(request),
+                  'icon_url': self.icon_url(request),
+                 }
+        registry = request.registry
+        items = result['items'] = [x(request) for x in
+                                    self.listItems(registry,
+                                                   filter_spec, sort_spec,
+                                                   batch_start, batch_size)]
+        if include_actions:
+            result['actions'] = [x(request) for x in self.actions]
+
+        if include_factories:
+            result['factories'] = [x(request) for x in self.actions]
+
+        return result
+
+
+def _get_title(context):
+     result = getattr(context, 'title', context)
+     if result is context:
+         result = context.__name__ or 'Root'
+     return result
+
+
+def _get_modified(context):
+    result = getattr(context, 'modified', context)
+    if result is context:
+        result = _now()
+    return result
---
+++ bottlecap/bottlecap/tests/test_adapters.py
@@ -0,0 +1,216 @@
+import unittest
+
+class FolderContainerInfoTests(unittest.TestCase):
+
+    def setUp(self):
+        from pyramid.testing import setUp
+        self.config = setUp()
+        self._set_NOW(None)
+
+    def tearDown(self):
+        from pyramid.testing import tearDown
+        self._set_NOW(None)
+        tearDown()
+
+    def _getTargetClass(self):
+        from bottlecap.adapters import FolderContainerInfo
+        return FolderContainerInfo
+
+    def _makeOne(self, context=None):
+        if context is None:
+            context = self._makeContext()
+        return self._getTargetClass()(context)
+
+    def _makeContext(self, **kw):
+        from pyramid.testing import DummyModel
+        return DummyModel(**kw)
+
+    def _makeRequest(self, **kw):
+        from pyramid.testing import DummyRequest
+        return DummyRequest(**kw)
+
+    def _set_NOW(self, value):
+        from bottlecap import adapters
+        adapters._NOW = value
+
+    def _registerItemInfoAdapter(self):
+        from zope.interface import implements
+        from bottlecap.interfaces import IItemInfo
+        class _ItemInfo(object):
+            implements(IItemInfo)
+            def __init__(self, context):
+                self.context = context
+            def __call__(self, request):
+                return {'for': 'testing only'}
+        self.config.registry.registerAdapter(_ItemInfo, (None,), IItemInfo)
+        return _ItemInfo
+
+    def test_class_conforms_to_IContainerInfo(self):
+        from zope.interface.verify import verifyClass
+        from bottlecap.interfaces import IContainerInfo
+        verifyClass(IContainerInfo, self._getTargetClass())
+
+    def test_instance_conforms_to_IContainerInfo(self):
+        from zope.interface.verify import verifyObject
+        from bottlecap.interfaces import IContainerInfo
+        verifyObject(IContainerInfo, self._makeOne())
+
+    def test_title_context_wo_title_or_name(self):
+        adapter = self._makeOne()
+        self.assertEqual(adapter.title, 'Root')
+
+    def test_title_context_wo_title_w_name(self):
+        context = self._makeContext(__name__='testing')
+        adapter = self._makeOne(context)
+        self.assertEqual(adapter.title, 'testing')
+
+    def test_title_context_w_title_w_name(self):
+        context = self._makeContext(__name__='testing', title='TITLE')
+        adapter = self._makeOne(context)
+        self.assertEqual(adapter.title, 'TITLE')
+
+    def test_modified_context_wo_modified(self):
+        import datetime
+        WHEN = datetime.datetime(2011, 2, 2, 8, 1)
+        self._set_NOW(WHEN)
+        adapter = self._makeOne()
+        self.assertEqual(adapter.modified, WHEN)
+
+    def test_modified_context_w_modified(self):
+        import datetime
+        WHEN = datetime.datetime(2011, 2, 2, 8, 1)
+        context = self._makeContext(modified=WHEN)
+        adapter = self._makeOne(context)
+        self.assertEqual(adapter.modified, WHEN)
+
+    def test_creator_context_wo_creator(self):
+        adapter = self._makeOne()
+        self.assertEqual(adapter.creator, None)
+
+    def test_creator_context_w_creator(self):
+        context = self._makeContext(creator='phred')
+        adapter = self._makeOne(context)
+        self.assertEqual(adapter.creator, 'phred')
+
+    def test_actions(self):
+        adapter = self._makeOne()
+        self.assertEqual(len(adapter.actions), 0) # XXX
+
+    def test_factories(self):
+        adapter = self._makeOne()
+        self.assertEqual(len(adapter.factories), 0) # XXX
+
+    def test_filter_schema(self):
+        adapter = self._makeOne()
+        self.assertEqual(adapter.filter_schema, None) # XXX
+
+    def test_sort_schema(self):
+        adapter = self._makeOne()
+        self.assertEqual(adapter.sort_schema, None) # XXX
+
+    def test_parent_url_wo_parent(self):
+        adapter = self._makeOne()
+        request = self._makeRequest()
+        self.assertEqual(adapter.parent_url(request), None)
+
+    def test_parent_url_w_parent(self):
+        root = self._makeContext()
+        parent = root['parent'] = self._makeContext()
+        context = parent['context'] = self._makeContext()
+        adapter = self._makeOne(context)
+        request = self._makeRequest()
+        self.assertEqual(adapter.parent_url(request),
+                         'http://example.com/parent/')
+
+    def test_icon_url(self):
+        self.config.add_static_view('static', 'bottlecap:static')
+        adapter = self._makeOne()
+        request = self._makeRequest()
+        self.assertEqual(adapter.icon_url(request),
+                         'http://example.com/static/folder_icon.png')
+
+    def test_listItems_filter_spec_unsupported(self):
+        adapter = self._makeOne()
+        self.assertRaises(ValueError, adapter.listItems,
+                          self.config.registry, filter_spec=object())
+
+    def test_listItems_sort_spec_unsupported(self):
+        adapter = self._makeOne()
+        self.assertRaises(ValueError, adapter.listItems,
+                          self.config.registry, sort_spec=object())
+
+    def test_listItems_batch_size_wo_batch_start(self):
+        adapter = self._makeOne()
+        self.assertRaises(ValueError, adapter.listItems,
+                          self.config.registry, batch_size=10)
+
+    def test_listItems_empty_context(self):
+        adapter = self._makeOne()
+        self.assertEqual(len(adapter.listItems(self.config.registry)), 0)
+
+    def test_listItems_non_empty_context(self):
+        _ItemInfo = self._registerItemInfoAdapter()
+        registry = self.config.registry
+        context = self._makeContext()
+        for i in range(10):
+            context['item_%d' % i] = self._makeContext()
+        adapter = self._makeOne(context)
+        items = adapter.listItems(registry)
+        self.assertEqual(len(items), 10)
+        for item in items:
+            self.failUnless(isinstance(item, _ItemInfo))
+
+    def test_listItems_batch_start_wo_batch_size(self):
+        _ItemInfo = self._registerItemInfoAdapter()
+        registry = self.config.registry
+        context = self._makeContext()
+        for i in range(30):
+            context['item_%d' % i] = self._makeContext()
+        adapter = self._makeOne(context)
+        items = adapter.listItems(registry, batch_start=0)
+        self.assertEqual(len(items), 20)
+
+    def test___call___defaults_no_items(self):
+        import datetime
+        WHEN = datetime.datetime(2011, 2, 2, 8, 1)
+        self._set_NOW(WHEN)
+        self.config.add_static_view('static', 'bottlecap:static')
+        adapter = self._makeOne()
+        request = self._makeRequest()
+        info = adapter(request)
+        self.assertEqual(info['title'], 'Root')
+        self.assertEqual(info['modified'], WHEN)
+        self.assertEqual(info['parent_url'], None)
+        self.assertEqual(info['icon_url'], 
+                         'http://example.com/static/folder_icon.png')
+        self.assertEqual(len(info['items']),  0)
+        self.assertEqual(len(info['actions']),  0)      # XXX
+        self.assertEqual(len(info['factories']),  0)    # XXX
+
+    def test___call___w_items(self):
+        self._registerItemInfoAdapter()
+        self.config.add_static_view('static', 'bottlecap:static')
+        context = self._makeContext()
+        for i in range(10):
+            context['item_%d' % i] = self._makeContext()
+        adapter = self._makeOne(context)
+        request = self._makeRequest(registry=self.config.registry)
+        info = adapter(request)
+        items = info['items']
+        self.assertEqual(len(items), 10)
+        for item in items:
+            self.assertEqual(item['for'], 'testing only')
+
+    def test___call___wo_include_actions(self):
+        self.config.add_static_view('static', 'bottlecap:static')
+        adapter = self._makeOne()
+        request = self._makeRequest()
+        info = adapter(request, include_actions=False)
+        self.failIf('actions' in info)
+
+    def test___call___wo_include_factories(self):
+        self.config.add_static_view('static', 'bottlecap:static')
+        adapter = self._makeOne()
+        request = self._makeRequest()
+        info = adapter(request, include_factories=False)
+        self.failIf('factories' in info)
